c305d334a32b9f5473a3516630ba416308908388
Tauqeer <146410148+codeMaverick7770@users.noreply.github.com>
2024-07-17 12:42:18 +0530

    Create #problem2.java

---
+++ Day03/#problem2.java
@@ -0,0 +1,18 @@
+
+//169. Majority Element
+// Time Complexity: O(n) Space Complexity: O(n)
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        Integer candidate = null;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        return candidate;
+    }
+}

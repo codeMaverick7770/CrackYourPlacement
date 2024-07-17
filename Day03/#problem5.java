a1b37c104de9a9d6c84dd69ff971b48649372332
Tauqeer <146410148+codeMaverick7770@users.noreply.github.com>
2024-07-17 23:28:43 +0530

    Update #problem5.java

--- Day03/#problem5.java
+++ Day03/#problem5.java
@@ -1,1 +1,19 @@
-
+//560. Subarray Sum Equals K
+//Time Complexity: O(n) & Space: O(n)
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        map.put(0,1);
+        int sum = 0;
+        int count = 0;
+        for(int num:nums) {
+            sum+=num;
+            if(map.containsKey(sum-k)) {
+                count+=map.get(sum-k);
+            }
+            map.put(sum,map.getOrDefault(num,0)+1);
+        }
+        return count;
+        
+    }
+}

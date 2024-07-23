d50f5c30084878299edb754ec784348b099f27ec
Tauqeer <146410148+codeMaverick7770@users.noreply.github.com>
2024-07-23 23:41:57 +0530

    Create #problem4.java

---
+++ Day09/#problem4.java
@@ -0,0 +1,24 @@
+
+// Given an input string s, reverse the order of the words.
+// A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+// Return a string of the words in reverse order concatenated by a single space.
+// Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
+//Time Comp: O(n) & O(n)
+class Solution {
+    public String reverseWords(String s) {
+        // Trim the input string to remove leading and trailing spaces
+        String[] str = s.trim().split("\\s+");
+
+        // Initialize the output string
+        String out = "";
+
+        // Iterate through the words in reverse order
+        for (int i = str.length - 1; i > 0; i--) {
+            // Append the current word and a space to the output
+            out += str[i] + " ";
+        }
+
+        // Append the first word to the output (without trailing space)
+        return out + str[0];
+    }
+}
